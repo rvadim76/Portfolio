5a84d6534c4115f1fc663eed2ec4466c2bfee089
vrusu <rvadim76@gmail.com>
2024-10-29 14:52:26 -0500

    Day 22: Added description, image and links.

--- main.py
+++ main.py
@@ -24,16 +24,23 @@ Below you can find some of the apps I have built in Phyton (more to come). Pleas
 
 st.write(content2)
 
-col3, col4 = st.columns(2)
+# col3, empty_col, col4 = st.columns(3)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("C:/Users/vrusu/PycharmProjects/portfolio_app/venv/data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("C:/Users/vrusu/PycharmProjects/portfolio_app/venv/images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("C:/Users/vrusu/PycharmProjects/portfolio_app/venv/images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 
